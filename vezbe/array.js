a669eb516408101e05ea6fb4a6087d70fd6db5a9
BIT Student <classroom@bgit.rs>
2020-02-12 18:05:37 +0100

    komit1

---
+++ vezbe/array.js
@@ -0,0 +1,70 @@
+//1
+// var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+// var a = months[5];
+// var b = months[9];
+// var c = months[0];
+// var d = [a, b, c];
+// console.log(a, b, c)
+
+//2
+// var days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+// var a = days[6];
+// console.log(a);
+
+//3
+//var array = [2, -4, 5, -2, -11];
+
+// var negA = array[1];
+// var negB = array[3];
+// var negC = array[4];
+
+// console.log(negA, negB, negC)
+
+//3///////drugi nacin
+// if (array[0] < 0) {
+//     console.log(array[0])
+// }
+// if (array[1] < 0) {
+//     console.log(array[1])
+// }
+// if (array[2] < 0) {
+//     console.log(array[2])
+// }
+// if (array[3] < 0) {
+//     console.log(array[3])
+// }
+// if (array[4] < 0) {
+//     console.log(array[4])
+// }
+
+//4
+// var a = [5, 15, -5, 20, 12, 18, 72, 14, 9];
+
+// if (a[0] % 3 === 0) {
+//     console.log(a[0]);
+// }
+// if (a[1] % 3 === 0) {
+//     console.log(a[1]);
+// }
+// if (a[2] % 3 === 0) {
+//     console.log(a[2]);
+// }
+// if (a[3] % 3 === 0) {
+//     console.log(a[3]);
+// }
+// if (a[4] % 3 === 0) {
+//     console.log(a[4]);
+// }
+// if (a[5] % 3 === 0) {
+//     console.log(a[5])
+// }
+// if (a[6] % 3 === 0) {
+//     console.log(a[6]);
+// }
+// if (a[7] % 3 === 0) {
+//     console.log(a[7])
+// }
+// if (a[8] % 3 === 0) {
+//     console.log(a[8]);
+// }
+
---
+++ vezbe/petlje.js
@@ -0,0 +1,248 @@
+//1
+// var result = '';
+// var a = 3;
+
+// switch (a) {
+//     case 1:
+//         result = 'Monday';
+//         break;
+//     case 2:
+//         result = "Tuesday";
+//         break;
+//     case 3:
+//         result = "Wednesday";
+//         break;
+//     case 4:
+//         result = "Thursday";
+//         break;
+//     case 5:
+//         result = "Friday";
+//         break;
+//     case 6:
+//         result = "Saturday";
+//         break;
+//     case 7:
+//         result = "Sunday";
+//         break;
+//     default:
+//         result = "Invalid"
+//         break;
+// }
+// console.log(result);
+
+//2
+// var a = 8;
+// var result = '';
+
+// switch (a) {
+//     case 1:
+//         result = "Monday";
+//         break;
+//     case 2:
+//         result = "Tuesday";
+//         break;
+//     case 3:
+//         result = "Wednesday";
+//         break;
+//     case 4:
+//         result = "Thursday";
+//         break;
+//     case 5:
+//         result = "Friday";
+//         break;
+//     case 6:
+//         result = "Saturday";
+//         break;
+//     case 7:
+//         result = "Sunday";
+//         break;
+//     default:
+//         result = "Input must be a number between 1 and 7";
+//         break;
+// }
+// console.log(result)
+
+//3
+// var a = 6;
+// var result = '';
+
+// switch (a) {
+//     case 1:
+//     case 2:
+//     case 3:
+//     case 4:
+//     case 5:
+//         result = "It is weekday"
+//         break;
+//     case 6:
+//     case 7:
+//         result = "It is weekend"
+//         break;
+//     default:
+//         result = "Input must be number between 1 and 7";
+//         break;
+// }
+// console.log(result)
+
+//4
+// var a = 4;
+// var result = ""
+
+// switch (a) {
+//     case 1:
+//         result = "January";
+//         break;
+//     case 2:
+//         result = "February";
+//         break;
+//     case 3:
+//         result = "March";
+//         break;
+//     case 4:
+//         result = "April";
+//         break;
+//     case 5:
+//         result = "May";
+//         break;
+//     case 6:
+//         result = "June";
+//         break;
+//     case 7:
+//         result = "July";
+//         break;
+//     case 8:
+//         result = "August";
+//         break;
+//     case 9:
+//         result = "September";
+//         break;
+//     case 10:
+//         result = "October";
+//         break;
+//     case 11:
+//         result = "November";
+//         break;
+//     case 12:
+//         result = "December";
+//         break;
+//     default:
+//         result = "Input must be a number between 1 and 12";
+//         break;
+
+// }
+// console.log(result)
+
+//5
+// var a = 1;
+// var result = "";
+
+// switch (a) {
+//     case 12:
+//     case 1:
+//     case 2:
+//         result = "Winter";
+//         break;
+//     case 3:
+//     case 4:
+//     case 5:
+//         result = "Spring";
+//         break;
+//     case 6:
+//     case 7:
+//     case 8:
+//         result = "Summer";
+//     case 9:
+//     case 10:
+//     case 11:
+//         result = "Autumn";
+//         break;
+//     default:
+//         result = "It must be a number between 1 and 12";
+//         break;
+// }
+// console.log(result)
+
+//6
+// var a = "A";
+// var result = "";
+// switch (a) {
+//     case "A":
+//         result = "Good job";
+//         break;
+//     case "B":
+//         result = "Pretty good";
+//         break;
+//     case "C":
+//         result = "Passed";
+//         break;
+//     case "D":
+//         result = "Not so good";
+//         break;
+//     case "F":
+//         result = "Failed";
+//         break;
+//     default:
+//         result = "Unknown grade";
+//         break;
+// }
+// console.log(result);
+
+//7
+// var a = "Beograd";
+// var result = "";
+
+// switch (a) {
+//     case "Dortmund":
+//     case "Berlin":
+//     case "Munich":
+//         result = "Germany";
+//         break;
+//     case "Paris":
+//     case "Lyon":
+//     case "Marseille":
+//         result = "France";
+//         break;
+//     case "Beograd":
+//     case "Trgoviste":
+//     case "Cuprija":
+//         result = "Srbija";
+//         break;
+//     case "Moscow":
+//     case "St.Petersburg":
+//     case "Tula":
+//         result = "Russia";
+//         break;
+//     case "Madrid":
+//     case "Barcelona":
+//     case "Valencia":
+//         result = "Spain";
+//         break;
+//     default:
+//         result = "Please choose a different city";
+//         break;
+// }
+// console.log(result);
+
+// var a = 5;
+// var b = 4;
+// var c = "+";
+// result = 0;
+
+// switch (a, b, c) {
+//     case "+":
+//         result = a + b;
+//         break;
+//     case "-":
+//         result = a - b;
+//         break;
+//     case "/":
+//         result = a / b;
+//         break;
+//     case "*":
+//         result = a * b;
+//         break;
+//     default:
+//         result = 0;
+//         break;
+// }
+// console.log(result);
