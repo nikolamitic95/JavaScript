b1ffc04c72b1dae4a83810815f4a924dd96af7c8
BIT Student <classroom@bgit.rs>
2020-02-13 18:02:39 +0100

    petlje

--- vezbe/petlje.js
+++ vezbe/petlje.js
@@ -1,248 +1,62 @@
-//1
-// var result = '';
-// var a = 3;
+// for (var a = 10; a >= 1; a--) {
+//     console.log(a);
 
-// switch (a) {
-//     case 1:
-//         result = 'Monday';
-//         break;
-//     case 2:
-//         result = "Tuesday";
-//         break;
-//     case 3:
-//         result = "Wednesday";
-//         break;
-//     case 4:
-//         result = "Thursday";
-//         break;
-//     case 5:
-//         result = "Friday";
-//         break;
-//     case 6:
-//         result = "Saturday";
-//         break;
-//     case 7:
-//         result = "Sunday";
-//         break;
-//     default:
-//         result = "Invalid"
-//         break;
 // }
-// console.log(result);
+
+//1
+// for (var a = 1; a <= 15; a++) {
+//     if (a % 2 === 0) {
+//         console.log("Broj " + a + " je paran")
+//     } else {
+//         console.log("Broj " + a + " je neparan")
+//     }
+// }
 
 //2
-// var a = 8;
-// var result = '';
-
-// switch (a) {
-//     case 1:
-//         result = "Monday";
-//         break;
-//     case 2:
-//         result = "Tuesday";
-//         break;
-//     case 3:
-//         result = "Wednesday";
-//         break;
-//     case 4:
-//         result = "Thursday";
-//         break;
-//     case 5:
-//         result = "Friday";
-//         break;
-//     case 6:
-//         result = "Saturday";
-//         break;
-//     case 7:
-//         result = "Sunday";
-//         break;
-//     default:
-//         result = "Input must be a number between 1 and 7";
-//         break;
+// var result = 0;
+// for (var b = 0; b < 1000; b++) {
+//     if (b % 3 === 0 && b % 5 === 0) {
+//         result = result + b;
+//     }
 // }
 // console.log(result)
 
 //3
-// var a = 6;
-// var result = '';
+// var a = [2, 5, 6, 7, 10];
+// var sum = 0;
+// var multi = 1;
 
-// switch (a) {
-//     case 1:
-//     case 2:
-//     case 3:
-//     case 4:
-//     case 5:
-//         result = "It is weekday"
-//         break;
-//     case 6:
-//     case 7:
-//         result = "It is weekend"
-//         break;
-//     default:
-//         result = "Input must be number between 1 and 7";
-//         break;
+// for (var i = 0; i < a.length; i++) {
+//     sum = sum + a[i];
+//     multi = multi * a[i];
 // }
-// console.log(result)
+// console.log(sum, multi);
 
 //4
-// var a = 4;
-// var result = ""
+// var x = ['1', 'A', 'B', "c", "r", true, NaN, undefined];
+// var y = "";
 
-// switch (a) {
-//     case 1:
-//         result = "January";
-//         break;
-//     case 2:
-//         result = "February";
-//         break;
-//     case 3:
-//         result = "March";
-//         break;
-//     case 4:
-//         result = "April";
-//         break;
-//     case 5:
-//         result = "May";
-//         break;
-//     case 6:
-//         result = "June";
-//         break;
-//     case 7:
-//         result = "July";
-//         break;
-//     case 8:
-//         result = "August";
-//         break;
-//     case 9:
-//         result = "September";
-//         break;
-//     case 10:
-//         result = "October";
-//         break;
-//     case 11:
-//         result = "November";
-//         break;
-//     case 12:
-//         result = "December";
-//         break;
-//     default:
-//         result = "Input must be a number between 1 and 12";
-//         break;
-
+// for (var i = 0; i < x.length; i++) {
+//     y = y + x[i];
 // }
-// console.log(result)
+// console.log(y);
 
 //5
-// var a = 1;
-// var result = "";
-
-// switch (a) {
-//     case 12:
-//     case 1:
-//     case 2:
-//         result = "Winter";
-//         break;
-//     case 3:
-//     case 4:
-//     case 5:
-//         result = "Spring";
-//         break;
-//     case 6:
-//     case 7:
-//     case 8:
-//         result = "Summer";
-//     case 9:
-//     case 10:
-//     case 11:
-//         result = "Autumn";
-//         break;
-//     default:
-//         result = "It must be a number between 1 and 12";
-//         break;
+// var a = [
+//     [1, 2, 1, 24],
+//     [8, 11, 9, 4],
+//     [7, 0, 7, 27]
+// ];
+// for (var i = 0; i < a.length; i++) {
+//     for (var b = 0; b < a[i].length; b++) {
+//         console.log(a[i][b]);
+//     }
 // }
-// console.log(result)
 
 //6
-// var a = "A";
-// var result = "";
-// switch (a) {
-//     case "A":
-//         result = "Good job";
-//         break;
-//     case "B":
-//         result = "Pretty good";
-//         break;
-//     case "C":
-//         result = "Passed";
-//         break;
-//     case "D":
-//         result = "Not so good";
-//         break;
-//     case "F":
-//         result = "Failed";
-//         break;
-//     default:
-//         result = "Unknown grade";
-//         break;
-// }
-// console.log(result);
+var sum = 0;
 
-//7
-// var a = "Beograd";
-// var result = "";
-
-// switch (a) {
-//     case "Dortmund":
-//     case "Berlin":
-//     case "Munich":
-//         result = "Germany";
-//         break;
-//     case "Paris":
-//     case "Lyon":
-//     case "Marseille":
-//         result = "France";
-//         break;
-//     case "Beograd":
-//     case "Trgoviste":
-//     case "Cuprija":
-//         result = "Srbija";
-//         break;
-//     case "Moscow":
-//     case "St.Petersburg":
-//     case "Tula":
-//         result = "Russia";
-//         break;
-//     case "Madrid":
-//     case "Barcelona":
-//     case "Valencia":
-//         result = "Spain";
-//         break;
-//     default:
-//         result = "Please choose a different city";
-//         break;
-// }
-// console.log(result);
-
-// var a = 5;
-// var b = 4;
-// var c = "+";
-// result = 0;
-
-// switch (a, b, c) {
-//     case "+":
-//         result = a + b;
-//         break;
-//     case "-":
-//         result = a - b;
-//         break;
-//     case "/":
-//         result = a / b;
-//         break;
-//     case "*":
-//         result = a * b;
-//         break;
-//     default:
-//         result = 0;
-//         break;
-// }
-// console.log(result);
+for (var i = 0; i <= 20; i++) {
+    sum = sum + i * i;
+}
+console.log(sum);
