b7ce83e447cc7514b515991d10946a323db39f47
BIT Student <classroom@bgit.rs>
2020-02-14 17:37:05 +0100

    conditions

---
+++ vezbe/conditions.js
@@ -0,0 +1,171 @@
+/*********Task 1*************/
+
+// var a = 3;
+// var b = -7;
+// var c = 2;
+// var product = a * b * c;
+// var result = "";
+// if (product > 0) {
+//     result = "The sign is +";
+// } else {
+//     result = "The sign is -";
+// }
+// console.log(result);
+
+/******** Task 2 - Verzija 1*****/
+
+// var a, b, c, d, e;
+// a = -5;
+// b = -2;
+// c = -6;
+// d = 0;
+// e = -1;
+// var result = '';
+// if (a > b && a > c && a > d && a > e) {
+//     result = a;
+// }
+// if (b > c && b > d && b > e) {
+//     result = b;
+// }
+// if (c > d && c > e) {
+//     result = c;
+// }
+
+// if (d > e) {
+//     result = d;
+// } else {
+//     result = e;
+// }
+// console.log(result);
+
+/******** Task 2 - Verzija 2*****/
+
+// var a, b, c, d, e;
+// a = -5;
+// b = -2;
+// c = -6;
+// d = 0;
+// e = -1;
+// var result = '';
+// if (a > b && a > c && a > d && a > e) {
+//     result = a;
+// } else if (b > c && b > d && b > e) {
+//     result = b;
+// } else if (c > d && c > e) {
+//     result = c;
+// } else if (d > e) {
+//     result = d;
+// } else {
+//     result = e;
+// }
+// console.log(result);
+
+/*********** Task 3 **************/
+
+// var a = 0;
+// var b = -1;
+// var c = 4;
+
+// if (a > b && b > c) {
+//     console.log(a, b, c);
+// } else if (a > c && c > b) {
+//     console.log(a, c, b);
+// } else if (b > a && a > c) {
+//     console.log(b, a, c);
+// } else if (b > c && c > a) {
+//     console.log(b, c, a);
+// } else if (c > a && a > b) {
+//     console.log(c, a, b);
+// } else if (c > b && b > a) {
+//     console.log(c, b, a);
+// } else {
+//     console.log("I give up!");
+// }
+
+/*************Task 4********/
+
+// var a = 10;
+
+// if (typeof a === 'number' && a % 2 === 0) {
+//     console.log(a / 2); // 10 / 2 = 5
+// } else {
+//     console.log("x")
+// }
+
+/*************Task 5*********** */
+
+// var firstNumber = 190;
+// var secondNumber = 15;
+// var result = ""
+
+// if (firstNumber > secondNumber) {
+//     result = "The first number is higher."
+// } else {
+//     result = "The second number is higher"
+// }
+
+// console.log(result);
+
+/************* Task 6: *****************/
+
+// var c = 60;
+
+// var fahrenheit = c / 5 * 9 + 32;
+// var celsius = (fahrenheit - 32) / 9 * 5;
+
+// console.log(fahrenheit);
+// console.log(celsius);
+
+/******************Task 7*****************/
+
+// var a = 100;
+// var number = 13;
+// var result = 0;
+
+// if (a > number) {
+//     result = (a - number) * 2
+// } else {
+//     result = number - a;
+// }
+
+// console.log(result);
+
+/*******************Task 8*****************/
+// var firstNumber = 12;
+// var secondNumber = 5;
+// result = ""
+
+// if (firstNumber === secondNumber) {
+//     result = (firstNumber + secondNumber) * 3;
+// } else {
+//     result = firstNumber + secondNumber
+// }
+// console.log(result);
+
+/********************Task 9************/
+/*var firstNumber = 6;
+var secondNumber = 50;
+var result = ""
+
+if (firstNumber === 50 || secondNumber === 50 || firstNumber + secondNumber === 50) {
+    result = "true";
+} else {
+    result = "-"
+}
+
+console.log(result);*/
+
+/***********Task 10**********/
+// var number = 10;
+// var result = '';
+// if (number >= 20 && number <= 400) {
+//     if (number <= 100) {
+//         result = "20<=>100";
+//     } else {
+//         result = "100<=>400"
+//     }
+// } else {
+//     result = "-"
+// }
+
+// console.log(result);
