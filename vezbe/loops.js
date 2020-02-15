1c43f8797e21c3a5bac60fc4ccde176967c8024d
BIT Student <classroom@bgit.rs>
2020-02-15 17:47:49 +0100

    loops_drugi

--- vezbe/loops.js
+++ vezbe/loops.js
@@ -109,6 +109,33 @@
 
 
 
+/**********************111***********************/
+
+
+
+
+// var e = 3;
+// var a = [5, -4, 2, 3, 7];
+// res = "No";
+// for (i = 0; i < a.length; i++) {
+//     if (e === a[i]) {
+//         res = "Yes";
+//     }
+// }
+// console.log(res);
+
+
+/********222222****** */
+// var a = [-3, 11, 5, 3, 4, -8];
+// var res = [];
+// for (var i = 0; i < a.length; i++) {
+//     if (a[i] > 0) {
+//         res[i] = a[i] * 2;
+//     } else {
+//         res[i] = a[i];
+//     }
+// }
+// console.log(res);
 
 
 
@@ -116,25 +143,63 @@
 
 
 
+///////3///////
+// var a = [4, 2, 2, -1, 6];
+// var index = 0;
+// var min = a[index];
+// for (var i = 0; i < a.length; i++) {
+//     if (a[i] < min) {
+//         min = a[i];
+//         index = i;
+//     }
+// }
+// console.log(index, min);
 
 
 
+/***********55555****** */
+// var a = [3, 11, -5, -3, 2];
+// sum = 0;
+// for (i = 0; i < a.length; i++){
+//     if (a[i] > 0) {
+//         sum = sum + a[i];
+//     }
+// }
+
+// console.log(sum);
 
 
 
+///////9////////
+
+
+// var a = [4, 6, 2, 8, 2, 2];
+// var e = 2;
+// var res = [];
+// for (var i = 0; i < a.length; i++){
+//     if (a[i] !== e ){
+//        res[res.length] = a[i];
+//     }
+// }
+// console.log(res);
 
 
 
+///////********10**** */
 
 
 
-
-
-
-
-
-
-
+// var a = [2, -2, 33, 12, 5, 8];
+// var p = 3;
+// var e = 78;
+// var res = [];
+// for (var i = 0; i < a.length; i++) {
+//     if (p === i) {
+//         res[p] = e;
+//     }
+//     res[res.length] = a[i];
+// }
+// console.log(res);
 
 
 
