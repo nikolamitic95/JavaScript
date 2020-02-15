87571336f7f9b6aabf34d8ad4c75447bc95edbe5
BIT Student <classroom@bgit.rs>
2020-02-15 16:01:52 +0100

    loops

---
+++ vezbe/loops.js
@@ -0,0 +1,142 @@
+
+// var res = "";
+// var shape = "";
+// for (var i = 0; i < 3; i++) {
+//     res = res + "* "
+//     shape = shape + res + "\n";
+// }
+// console.log(shape);
+
+
+
+// var a = [3, 4, 5];
+// var res = "";
+// for (i = 0; i < a.length; i++) {
+//     res = res + a[i] + "\n";
+// }
+// console.log(res);
+
+
+
+
+// var a = [3, 4, false, 5];
+// var copy = [];
+// for (i = 0; i < a.length; i++) {
+//     if (typeof a[i] === "number") {
+//         copy[copy.length] = a[i] * 2;
+//     }
+// }
+// console.log(copy);
+
+
+// var a = [3, 8, 6];
+// var copy = [];
+// for (i = 0; i < a.length; i++){
+//     if (a[i] < 7){
+//         copy[copy.length] = a[i]; 
+//     }
+// }
+// console.log(copy);
+
+
+// var a = [3, 8, 7, false, "true", 6];
+// var res = 0;
+// for (i = 0; i < a.length; i++) {
+//     if (typeof a[i] === "number" && a[i] <= 6) {
+//         res = res + a[i];
+//     }
+
+// }
+// console.log(res);
+
+
+
+// var first = [3, 4, false];
+// var second = [2, 5, 8];
+// res = [];
+// for (i = 0; i < first.length; i++){
+//      res[res.length] = first[i];
+//      res[res.length] = second[i];
+// }
+
+// console.log(res);
+
+
+
+
+
+// var first = [3, 4, false];
+// var second = [2, 5, 8];
+// res = [];
+// for (i = 0; i < first.length; i++) {
+//     res[res.length] = first[i];
+// }
+// for (a = 0; a < second.length; a++) {
+//     res[res.length] = second[a];
+// }
+// console.log(res);
+
+
+
+// // var first = [3, 4, false];
+// // var second = [2, 5, 8];
+// // res = [];
+// // for (i = 0; i < first.length; i++) {
+// //     res[i] = first[i];
+// //     res[i + first.length] = second[i];
+// // }
+// // console.log(res);
+
+
+
+
+
+
+
+
+// var res = "";
+// var dim = 5;
+// var stars = "";
+// for (var i = 0; i < dim; i++) {
+//     res = res + "* ";
+// }
+// for (var a = 0; a < dim; a++) {
+//     stars = stars + res + "\n";
+// }
+
+// console.log(stars);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ vezbe/subota.js
@@ -0,0 +1,32 @@
+// var a = [80, 77, 88, 95, 68];
+// var sum = 0
+// for (var i = 0; i < a.length; i++) {
+//     sum = sum + a[i];
+// }
+// var grade = sum / a.length;
+// if (grade < 60) {
+//     console.log("F");
+// }
+// else if (grade < 70 && grade > 60) {
+//     console.log("D");
+// }
+// else if (grade < 80 && grade > 70) {
+//     console.log("C");
+
+// }
+// else if (grade < 90 && grade > 80) {
+//     console.log("B");
+// }
+// else {
+//     console.log("A");
+// }
+
+
+
+var res = "";
+for (var i = 0; i < 4; i++) {
+    res = res + "*\t"
+    console.log(res);
+}
+
+
