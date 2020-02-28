17db1af76add87459902a8287650612257f5a7bd
nikola.mitic1995@gmail.comgit config --global user.name Nikolagit congig --global user.email nikola.mitic1995@gmail.com <you@example.com>
2020-02-28 01:09:44 +0100

    metode

--- practicle/built_in_objects.js
+++ practicle/built_in_objects.js
@@ -1,8 +1,4 @@
 
-// Write a functional expression that duplicates each element of a given array.
-// Input: [2, 4, 7, 11, -2, 1]
-// Output: [2, 2, 4, 4, 7, 7, 11, 11,  -2, -2, 1, 1]
-
 // function duplicate(arr) {
 //     var arr1 = [];
 //     for (var i = 0; i < arr.length; i++) {
@@ -16,10 +12,6 @@
 // console.log(res);
 
 /////////////////// 2 ///////////////////
-// Write a functional expression that removes all duplicates in a given array.
-// Input: [8, 13, 8, 9, 12, 8, 1, 1, 4, 13]
-// Output: [1, 4, 8, 9, 12, 13] 
-
 
 // function removeElement(arr) {
 //     var arr1 = [];
@@ -39,9 +31,7 @@
 // console.log(res);
 
 ////////////////  3 ////////////////
-// Write a function that checks if a given array has odd number of elements.
-// Input: [1, 2, 9, 2, 1]
-// Output: true
+
 // function oddNumber(arr) {
 //     return arr.length % 2 !== 0
 // }
@@ -49,9 +39,7 @@
 // console.log(res);
 
 ///////////// b ///////////////
-// Write a function that counts the number of elements less than the middle element. If the given array has an even number of elements, print out an error message. 
-// Input: [-1, 8.1, 3, 6, 2.3, 44, 2.11]
-// Output: 4
+
 // function middleElement(arr) {
 //     var count = 0;
 //     if (arr.length % 2 === 0) {
@@ -70,23 +58,138 @@
 // console.log(res);
 
 //////////// 4 ////////////////
-// Write a function that finds the smallest element of a given array. The function should return an object that contains the smallest value and its last position in the array.
-// Input: [1, 4, -2, 11, 8, 1, -2, 3]
-// Output:  { minValue: -2, minLastIndex: 6 }
 
 
-// function smallest(arr) {
+// var smallest = function (arr) {
 //     var min = arr[0];
-//     var index;
+//     var index = 0;
 //     for (var i = 0; i < arr.length; i++) {
-//         if (arr[i] < arr[0]) {
+//         if (arr[i] < min) {
 //             min = arr[i];
 //             index = i;
 //         }
 //     }
-//     return min, index;
+//     return [min, index];
 // }
 
-// var res = smallest([1, 4, -2, 11, 8, 1, -2, 3]);
+// var res = new smallest([1, 4, -2, 11, 8, 1, -2, 3]);
 // console.log(res);
+
+
+/////////// 5.a ////////////
+
+
+// function small(arr, a){
+//     var arr1 = [];
+//     for(var i = 0; i<arr.length;i++){
+//         if(arr[i] < a){
+//             arr1[arr1.length] = arr[i];
+//         }
+//     }
+//     return arr1
+// }
+
+// var res = small([2, 3, 8, -2, 11, 4], 6);
+// console.log(res);
+
+
+////////// 5.b /////////
+
+
+// var pro =  function (arr, b){
+//    for(var i = 0; i<arr.length;i++){
+//        var arr1 = [];
+//        arr[i].toUpperCase();
+//        if(arr[i].slice(1, 3) === b){
+//             arr1[arr.length] = arr[i];
+//        }
+//    }
+//    return arr1
+
+// }
+// var res = pro(["JavaScript", "Programming", "fun", "product"], "pro");
+// console.log(res);
+
+
+///////// 5.c ////////
+
+
+/////// 6.a ////////
+
+// var product = [
+//     {
+//         name: "apples",
+//         price: 100
+//     },
+//     {
+//         name: "milk",
+//         price: 80
+//     },
+//     {
+//         name: "bananas",
+//         price: 150
+//     }
+// ]
+
+
+///////b///////
+
+
+// function sum(arr) {
+//     var res = 0;
+//     for (var i = 0; i < arr.length; i++) {
+//         res += arr[i].price;
+//     }
+//     return res;
+// }
+// var result = sum(product);
+// console.log(result);
+
+//////c//////
+
+
+// function average(arr1) {
+//     var res = 0;
+//     for (var i = 0; i < arr1.length; i++) {
+//         res += arr1[i].price;
+//     }
+//     return res / arr1.length;
+// }
+// var result = average(product);
+// console.log(result);
+
+
+/////d////////
+
+// function expensive(arr) {
+//     var max = arr[0].price;
+//     var index = 0;
+//     for (var i = 0; i < arr.length; i++) {
+//         if (max < arr[i].price) {
+//             max = arr[i].price;
+//             index = i;
+//         }
+        
+//     }
+//     return arr[index].name.toUpperCase();
+// }
+//   var result = expensive(product);
+//   console.log(result);
+
+
+//////// 7.a /////////
+
+// function str(str1){
+//    if(str1 === str1.toUpperCase()){
+//        return true;
+//    }
+//    return false;
+    
+// }
+
+// var result = str("niKoLa");
+// console.log(result);
+
+//////// b ///////////
+
 
